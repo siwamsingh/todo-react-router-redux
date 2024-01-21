84586dc7d6a96e18755fbd9cd96ee3e3868608fe
Brett Baker <GitBaBB@MyBaBB.com>
2024-01-21 00:18:35 -0800

    adjust padding and sm: on test header final adjustment.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,11 +11,11 @@ export default function Header() {
                 <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                 <span className="CustomFont1 self-center text:sm xs:text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
-            <div className="flex items-center lg:order-2">
-                <a href="#" className="CustomFont1 text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm 
-                pl-[.4rem] xxs:pl-[.4rem] lg:pl-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
+            <div className="flex  items-center lg:order-2">
+                <a href="#" className="CustomFont1 text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-xs xs:text-sm sm:text-base
+                p-[.4rem] xxs:p-[.4rem] lg:p-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
                 
-                <a href="#" className="CustomFont1 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm 
+                <a href="#" className="CustomFont1 text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-xs  xs:text-sm sm:text-base
                 pl-[.4rem] lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
 
                 <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
